f0b17031ebea573518029667ff8d20cb127ad9be
Sidaarth <skanthavellu@gmail.com>
2026-09-15 15:58:47 +0800

    Prepare server for Render deployment

--- server.js
+++ server.js
@@ -340,6 +340,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`CropShield running at http://localhost:${PORT}`);
 });
